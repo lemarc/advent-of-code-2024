4f95093a90166a82f42a8f8bec74a8d941bca5d1
Marcus <>
2024-12-13 17:06:36 -0800

    day 11 part 2

--- day11/index.js
+++ day11/index.js
@@ -5,53 +5,16 @@ const sampleFile = './' + DAY + '/sample.txt'
 const inputFile = './' + DAY + '/input.txt'
 console.log('Advent of Code - ' + DAY + '\n')
 
+const history = {}
+
 function process(lines) {
 	const initial = lines[0].split(' ').map(v=>parseInt(v))
-	let numbers = initial.slice()
-
-	for (let b = 0; b < 25; b++) {
-		numbers = blink(numbers)
-	}
 
 	console.log('Part 1')
-	console.log('Numbers = ' + numbers.length + '\n')
-
-	//for (let i = 0; i < initial.length; i++) {
-	//	numbers = [initial[i]]
-	//
-	//}
-
-	const count = {}
-	numbers = [36869184]
-	count[numbers[0]] = 1
-	console.log(numbers)
-	for (let b = 0; b < 11; b++) {
-		numbers = blink(numbers)
-		//numbers.sort(function(a, b){return b - a});
-		//console.log(numbers)
-		for (const n of numbers) {
-			if (count[n] == null) {
-				count[n] = 1
-			} else {
-				count[n]++
-			}
-		}
-	}
-	console.log(count)
-	console.log(Object.keys(count).length)
+	console.log('Numbers = ' + blinkBetter(initial, 25) + '\n')
 
 	console.log('Part 2')
-	console.log('Stones = ' + 0 + '\n')
-}
-
-function blinkFast(numbers, blinks) {
-	let sum = 0;
-
-	for (let i = 0; i < numbers.length; i++) {
-		sum = reapply([numbers[i]], blinks, sum)
-	}
-
-	return sum
+	console.log('Stones = ' + blinkBetter(initial, 75) + '\n')
 }
 
 function blink(numbers) {
@@ -65,26 +28,63 @@ function blink(numbers) {
 }
 
 function apply(number) {
-	if (number == 0) {
-		return [1]
+	if (history[number] == null) {
+		if (number == 0) {
+			history[number] = [1]
+		} else {
+			const numStr = number + ''
+			if (numStr.length % 2 == 0) {
+				const mid = numStr.length / 2
+				const left = parseInt(numStr.slice(0, mid))
+				const right = parseInt(numStr.slice(mid, numStr))
+				history[number] = [left, right]
+			} else {
+				history[number] = [number * 2024]
+			}
+			
+		}
 	}
-	const numStr = number + ''
-	if (numStr.length % 2 == 0) {
-		const mid = numStr.length / 2
-		const left = parseInt(numStr.slice(0, mid))
-		const right = parseInt(numStr.slice(mid, numStr))
-		return [left, right]
-	}
-	return [number * 2024]
+	return history[number]
 }
 
 
+function blinkBetter(stones, blinks) {
+	let count = {}
+	for (const stone of stones) {
+		if (count[stone] == null) {
+			count[stone] = 1
+		} else {
+			count[stone]++
+		}
+	}
 
+	for (let b = 0; b < blinks; b++) {
+		let newCount = {}
+		for (const number in count) {
+			const split = apply(number)
+
+			for (const n of split) {
+				if (newCount[n] == null) {
+					newCount[n] = count[number]
+				} else {
+					newCount[n] += count[number]
+				}
+			}
+		}
+		count = newCount
+	}
+
+	let sum = 0
+	for (const number in count) {
+		sum += count[number]
+	}
+	return sum
+}
 
 processLines(sampleFile, process)
 
-//console.time('Execution time')
-//await processLines(inputFile, process)
-//console.timeEnd('Execution time')
+console.time('Execution time')
+await processLines(inputFile, process)
+console.timeEnd('Execution time')
 
 
